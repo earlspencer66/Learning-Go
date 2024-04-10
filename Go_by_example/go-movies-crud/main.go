af801c499d21622a7e7f1f61b3adcb54eae4866a
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-10 15:01:47 +0300

    Update movie
    Function added

--- Go_by_example/go-movies-crud/main.go
+++ Go_by_example/go-movies-crud/main.go
@@ -51,10 +51,24 @@ func getMovie (w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateMovie (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for item, index: range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]...) //delete the movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"] //new movie to have same ID as the deleted movie
+			movies = append(movies, movie)
+			json.NewEncoder().Encode(movie)
+		}
+	}
+}
+
 func createMovie(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
-	_ = json.NewDecoder(r.body).Decode(&movie)
+	_ = json.NewDecoder(r.Body).Decode(&movie)
 	movie.ID = strconv.Itoa(rand.Intn(100000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
