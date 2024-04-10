b3edcb933c5c9f460f9ea1830545ff5f456603e9
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-10 15:29:02 +0300

    Update, delete
    Order of index and item matters

--- Go_by_example/go-movies-crud/main.go
+++ Go_by_example/go-movies-crud/main.go
@@ -1,71 +1,75 @@
 package main
 
-import ("fmt"
-		"net/http"
-		"github.com/gorilla/mux"
-		"math/rand"
-		"strconv"
-		"encoding/json"
-		"log"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct {
-		ID string `json:"id"`
-		Isbn string `json:"isbn"`
-		Title string `json:"title"`
-		Director *Director `json:"director"`
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
 }
 
 type Director struct {
-		Firstname string `json:"firstname"`
-		Lastname string `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
-var movies [] Movie    //slice of type movie
+var movies []Movie //slice of type movie
 
-func getMovies(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-type", "application/json")
-		json.NewEncoder(w).Encode(movies)
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	json.NewEncoder(w).Encode(movies)
 }
 
-func deleteMovie (w http.ResponseWriter, r *http.Request){
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for item, index := range movies {
-		if item.ID == params["id"]{
+	for index, item := range movies {
+		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(movies)  // return the remaining movies
+	json.NewEncoder(w).Encode(movies) // return the remaining movies
 }
 
-func getMovie (w http.ResponseWriter, r *http.Request) {
+func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 	for _, item := range movies {
-		if item.ID = params["id"]{
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item) //return the requested movie
 			return
 		}
 	}
 }
 
-func updateMovie (w http.ResponseWriter, r *http.Request){
+func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for item, index: range movies {
-		if item.ID == params["id"]{
+	for index, item := range movies {
+		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...) //delete the movie
+			var movie Movie
 			_ = json.NewDecoder(r.Body).Decode(&movie)
 			movie.ID = params["id"] //new movie to have same ID as the deleted movie
 			movies = append(movies, movie)
-			json.NewEncoder().Encode(movie)
+			json.NewEncoder(w).Encode(movie)
+			return
 		}
 	}
 }
 
-func createMovie(w http.ResponseWriter, r *http.Request){
+func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
@@ -74,16 +78,16 @@ func createMovie(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(movie)
 }
 
-func main () {
+func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies, Movies{ID: "1", Isbn: "123456789", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "1", Isbn: "123456789", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "14578686", Title: "Movie Two", Director: &Director{Firstname: "Steven", Lastname: "Smith"}})
 	movies = append(movies, Movie{ID: "3", Isbn: "15265856", Title: "Movie Three", Director: &Director{Firstname: "Earl", Lastname: "Mogire"}})
 	movies = append(movies, Movie{ID: "4", Isbn: "56565562", Title: "Movie Four", Director: &Director{Firstname: "Spencer", Lastname: "Earl"}})
-	movies = append (movies, Movie{ID: "5", Isbn: "56586258", Title: "Movie Five", Director: &Director{Firstname: "Most", Lastname: "Excellent"}})
+	movies = append(movies, Movie{ID: "5", Isbn: "56586258", Title: "Movie Five", Director: &Director{Firstname: "Most", Lastname: "Excellent"}})
 	//need five different routes and five different functions -- see draw.io file
-	r.HandleFunc("/movies", getMovies).Methods("GET") 
+	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
