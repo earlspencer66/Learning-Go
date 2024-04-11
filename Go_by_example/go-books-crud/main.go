be0ae7b1b3b7906fde35f6dfd3dd1c01ca2e238c
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-11 13:22:26 +0300

    Get specific book
    Function added

--- Go_by_example/go-books-crud/main.go
+++ Go_by_example/go-books-crud/main.go
@@ -29,6 +29,15 @@ func getBooks(w http.ResponseWriter, r http.Request) {
 	json.NewEncoder(w).Encode(books)
 }
 
+func getBook(w http.ResponseWriter, r http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+		}
+}
+
 func main() {
 	r := mux.NewRouter()
 
