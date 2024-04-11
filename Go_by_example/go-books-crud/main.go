10fb91dff1c9d96ac5ade2925e2e916b76b2519b
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-11 13:16:39 +0300

    Routes
    All five routes added

--- Go_by_example/go-books-crud/main.go
+++ Go_by_example/go-books-crud/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -23,4 +27,17 @@ func main() {
 	r := mux.NewRouter()
 
 	books = append(books, Book{ID: "1", Isbn: "1563254", Title: "The Desire of Ages", Author: &Author{Firstname: "Ellen", Lastname: "White", Year: 1905}})
+
+	//need five different routes - see the flowchart
+	//"GET ALL", "GET BY ID", "CREATE", "UPDATE", "DELETE"
+	//with their corresponding methods
+
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/id", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/id", deleteBook).Methods("DELETE")
+	r.HandleFunc("/books/id", updateBook).Methods("PUT")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
