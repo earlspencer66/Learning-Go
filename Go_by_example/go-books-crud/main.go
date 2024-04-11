5bd7b100548b732f0722dc11c8d124fb24812ed1
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-11 13:45:26 +0300

    Update Book
    Function added

--- Go_by_example/go-books-crud/main.go
+++ Go_by_example/go-books-crud/main.go
@@ -62,6 +62,23 @@ func deleteBook (w http.http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(books)//return the remaining books
 }
 
+func updateBook (w http.http.ResponseWriter, r *http.Request) {
+	//basically delete and add book...to the same id
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books{
+		if item.ID == params ["id"]{
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
