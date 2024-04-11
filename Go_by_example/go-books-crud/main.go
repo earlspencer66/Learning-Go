4b7c6e1e72cab3a768f0174a367322634b82d5b0
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-11 13:39:50 +0300

    delete Books
    Function added

--- Go_by_example/go-books-crud/main.go
+++ Go_by_example/go-books-crud/main.go
@@ -27,6 +27,7 @@ var books []Book // slice of type books
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
+	return
 }
 
 func getBook(w http.ResponseWriter, r *http.Request){
@@ -35,6 +36,7 @@ func getBook(w http.ResponseWriter, r *http.Request){
 	for _, item := range books {
 		if item.ID == params["id"]{
 			json.NewEncoder(w).Encode(item)
+			return
 		}
 }
 
@@ -45,6 +47,19 @@ func createBook(w http.http.ResponseWriter, r *http.Request) {
 	book.ID = strconv.Ito(rand.Intn(100000000))
 	books = append(books, book)
 	json.NewEncoder(w).Encode(book)
+	return
+}
+
+func deleteBook (w http.http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"]{
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)//return the remaining books
 }
 
 func main() {
