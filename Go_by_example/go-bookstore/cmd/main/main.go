c87c453991ad1e41a14f0d9a7504e0a9b75a44c1
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-19 13:22:49 +0300

    Updated main.go

---
+++ Go_by_example/go-bookstore/cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jihnzu/gorm/dialects/mysql"
+	"github.com/pkg/routes"
+
+	//need to import the routes from the pkg folder
+	"C:/Users/earls/Documents/Learning-Go/Go_by_example/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.fatal(http.ListenAndServe("localhost:9010", r))
+}
