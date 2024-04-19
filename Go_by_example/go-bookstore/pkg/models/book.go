37317f2a9102e387a21a563e30d9c0c81a5f9668
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-19 13:14:35 +0300

    models
    for connection to the database

---
+++ Go_by_example/go-bookstore/pkg/models/book.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"C:/Users/earls/Documents/Learning-Go/Go_by_example/go-bookstore/pkg/config"
+
+	"github.com/jihnzu/gorm"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+// to connect to the database
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.Automigrate(&Book{})
+}
+
+// Logic
+// User interacts with the routes. Routes with the controllers
+// Controllers perform operations with the database
+
+//different functions for the controllers
