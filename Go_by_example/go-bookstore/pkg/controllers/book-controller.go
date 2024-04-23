a544d8d87d7279accf2fb63d646482c84b0e7071
Earl Spencer <62011828+earlspencer66@users.noreply.github.com>
2024-04-23 13:39:38 +0300

    Delete book controller function
    Added

--- Go_by_example/go-bookstore/pkg/controllers/book-controller.go
+++ Go_by_example/go-bookstore/pkg/controllers/book-controller.go
@@ -46,9 +46,25 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 // create book controller function
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	CreateBook := &models.Book{}
-	utils.ParseBosy(r, CreateBook)
+	utils.ParseBody(r, CreateBook)
 	b := CreateBook.CreateBook()
 	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+//delete book controller function
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing book") //same steps as getbook by ID
+	}
+	book := models.DeleteBook(D)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
